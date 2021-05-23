03ae052633e3c9a04cc96b01b5b6aae1d9af62d3
mdee <design4web2014@gmail.com>
2021-05-23 15:57:24 +0200

    connecting our app to database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+dotenv.config()
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+connectDB()
+
+const PORT = process.env.NODE_ENV || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`);
 })
