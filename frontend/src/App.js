1ffe2b4b43cb0d7f1518ed8e491d119d4fb790e1
mdee <design4web2014@gmail.com>
2021-05-23 12:12:05 +0200

    adding react-bootstrap for frontend and nodemon and concurrently for watching our server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'react-bootstrap'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant='primary'>Hello</Button>
       </header>
     </div>
   );
